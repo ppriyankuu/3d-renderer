839e8ef39054b7a2ff4a3592ea6b44f8de7f11e5
Priyanku Gogoi <workwithpriyanku@gmail.com>
2024-07-22 08:24:06 +0530

    defined vector types

--- src/headers/display.h
+++ src/headers/display.h
@@ -20,6 +20,7 @@ extern int window_height;
 // FUNCTION SIGNATURES
 bool initialize_window(void);
 
+void draw_pixel(int x, int y, uint32_t color);
 void draw_grid(void);
 void draw_rect(int x, int y, int width, int height, uint32_t color);
 
---
+++ src/headers/vector.h
@@ -0,0 +1,16 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+// structs to represent a postition vector
+typedef struct {
+    float x;
+    float y;
+} vec2_t;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} vec3_t;
+
+#endif
