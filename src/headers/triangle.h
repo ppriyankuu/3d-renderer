2a72d5ff387a61511fa6772970aa23fc5502f84a
Priyanku Gogoi <workwithpriyanku@gmail.com>
2024-08-27 17:18:56 +0530

    visualising textured triangles

--- src/headers/triangle.h
+++ src/headers/triangle.h
@@ -24,6 +24,11 @@ typedef struct {
 
 void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
+void draw_texel(
+    int x, int y, uint32_t* texture,
+    vec2_t point_a, vec2_t point_b, vec2_t point_c,
+    float u0, float v0, float u1, float v1, float u2, float v2
+);
 void draw_textured_triangle(
     int x0, int y0, float u0, float v0, 
     int x1, int y1, float u1, float v1, 
--- src/triangle.c
+++ src/triangle.c
@@ -61,6 +61,44 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
     }
 }
 
+vec3_t barycentric_weights(vec2_t a, vec2_t b, vec2_t c, vec2_t p) {
+    vec2_t ac = vec2_sub(c, a);
+    vec2_t ab = vec2_sub(b, a);
+    vec2_t ap = vec2_sub(p, a);
+    vec2_t pc = vec2_sub(c, p);
+    vec2_t pb = vec2_sub(b, p);
+
+    float area_parallelogram_abc = (ac.x * ab.y - ac.y * ab.x);
+
+    float alpha = (pc.x * pb.y - pc.y * pb.x) / area_parallelogram_abc;
+    float beta = (ac.x * ap.y - ac.y * ap.x) / area_parallelogram_abc;
+    float gamma = 1 - alpha - beta;
+
+    vec3_t weights = { alpha, beta, gamma };
+    return weights;
+}
+
+void draw_texel(
+    int x, int y, uint32_t* texture,
+    vec2_t point_a, vec2_t point_b, vec2_t point_c,
+    float u0, float v0, float u1, float v1, float u2, float v2
+) {
+    vec2_t point_p = { x, y };
+    vec3_t weights = barycentric_weights(point_a, point_b, point_c, point_p);
+
+    float alpha = weights.x;
+    float beta = weights.y;
+    float gamma = weights.z;
+
+    float interpolated_u = (u0) * alpha + (u1) * beta + (u2) * gamma;
+    float interpolated_v = (v0) * alpha + (v1) * beta + (v2) * gamma;
+
+    int tex_x = abs((int)(interpolated_u * texture_width));
+    int tex_y = abs((int)(interpolated_v * texture_height));
+
+    draw_pixel(x, y, texture[(texture_width * tex_y) + tex_x]);
+}
+
 void draw_textured_triangle(
     int x0, int y0, float u0, float v0, 
     int x1, int y1, float u1, float v1, 
@@ -88,6 +126,10 @@ void draw_textured_triangle(
         float_swap(&v0, &v1);
     }
 
+    vec2_t point_a = { x0, y0 };
+    vec2_t point_b = { x1, y1 };
+    vec2_t point_c = { x2, y2 };
+
     float inv_slope_1 = 0;
     float inv_slope_2 = 0;
 
@@ -104,7 +146,7 @@ void draw_textured_triangle(
             }
 
             for(int x = x_start; x < x_end; ++x){
-                draw_pixel(x, y, (x % 2 == 0 && y % 2 == 0) ? 0xFFFF00FF : 0xFF000000);
+                draw_texel(x, y, texture, point_a, point_b, point_c, u0, v0, u1, v1, u2, v2);
             }
         }
     }
@@ -125,7 +167,7 @@ void draw_textured_triangle(
             }
 
             for(int x = x_start; x < x_end; ++x){
-                draw_pixel(x, y, (x % 2 == 0 && y % 2 == 0) ? 0xFFFF00FF : 0xFF000000);
+                draw_texel(x, y, texture, point_a, point_b, point_c, u0, v0, u1, v1, u2, v2);
             }
         }
     }
